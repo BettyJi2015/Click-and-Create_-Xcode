3f25f356d7c62b46414522861aa6e72fd4ec432c
Ji Bei <Betty@140.72.208.web-pass.com>
2015-12-06 15:57:25 -0800

    Initial Commit

---
+++ Click and Create iosTests/Click_and_Create_iosTests.swift
@@ -0,0 +1,36 @@
+//
+//  Click_and_Create_iosTests.swift
+//  Click and Create iosTests
+//
+//  Created by Ji Bei on 15/12/6.
+//  Copyright © 2015年 Betty Ji. All rights reserved.
+//
+
+import XCTest
+@testable import Click_and_Create_ios
+
+class Click_and_Create_iosTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
